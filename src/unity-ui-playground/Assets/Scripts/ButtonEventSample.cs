ff1de44e2fe9b3a98e9ac65f9108e7a0ba018bf7
Denis Belavin <denis.belavin@saritasa.com>
2026-03-03 16:47:26 +0700

    Create data binding example UTD-606

--- src/unity-ui-playground/Assets/Scripts/ButtonEventSample.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// Sample class that demonstrates how to bind to a UI Toolkit button click event.
-/// </summary>
-public class ButtonEventSample : MonoBehaviour
-{
-
-}
---
+++ src/unity-ui-playground/Assets/Scripts/TimeViewModel.cs
@@ -0,0 +1,55 @@
+using System.Globalization;
+using Unity.Properties;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+/// <summary>
+/// Sample class that demonstrates how to bind to a UI Toolkit button click event.
+/// </summary>
+public class TimeViewModel : MonoBehaviour
+{
+    [SerializeField]
+    private UIDocument uiDocument;
+
+    private TimeModel model;
+    private Button btn_UpdateTime;
+
+    private void Awake()
+    {
+        var root = uiDocument.rootVisualElement;
+
+        model = new TimeModel();
+        root.dataSource = model;
+
+        btn_UpdateTime = root.Q<Button>();
+
+        btn_UpdateTime.clicked += OnClicked;
+    }
+
+    private void OnDestroy()
+    {
+        btn_UpdateTime.clicked -= OnClicked;
+    }
+
+    private void OnClicked()
+    {
+        model.UpdateTime();
+    }
+}
+
+/// <summary>
+/// Modal model class that contains the data to be displayed in the UI.
+/// </summary>
+public class TimeModel
+{
+    [CreateProperty]
+    private string time;
+
+    /// <summary>
+    /// Updates the time property with the current time.
+    /// </summary>
+    public void UpdateTime()
+    {
+        time = Time.time.ToString(CultureInfo.InvariantCulture);
+    }
+}
